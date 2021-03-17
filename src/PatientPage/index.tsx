e7647fcd7f695236ac67a5160ac680730ce6f333
Rami Piik <rami.piik@gmail.com>
2021-03-18 00:57:41 +0200

    9.22: patientor, step7

--- src/PatientPage/index.tsx
+++ src/PatientPage/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Patient } from "../types";
+import { Patient, Entry, HealthCheckEntry, OccupationalHealthCareEntry, HospitalEntry } from "../types";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
 import { useStateValue } from "../state";
 import {updatePatient} from "../state/reducer";
-import { Icon } from 'semantic-ui-react';
+import { Icon, Table, Container } from 'semantic-ui-react';
 
 
 const fetchPatientInfo = async (id:string) => {
@@ -48,22 +48,132 @@ const PatientPage = () => {
         );
     };
 
+    const HospitalEntry = ({ entry }: { entry: HospitalEntry }) => {
+        return (
+            <div>
+            <Table>
+                <Table.Body>
+                    <ul style={{marginTop:10}} key={entry.id}>
+                    <Table.Row>
+                        <Icon style={{marginLeft:-25, marginBottom:10}} name='hospital' size="large"/> <b>{entry.type}</b>
+                    </Table.Row>
+                    <Table.Row>
+                        <li>Date: {entry.date}</li>
+                    </Table.Row>
+                    <Table.Row>
+                        <li>Description: {entry.description}</li>
+                        {entry.diagnosisCodes && <li>Diagnosis codes: {diagnosisCodes (entry.diagnosisCodes)}</li>}
+                    </Table.Row>
+                </ul>
+                </Table.Body>
+            </Table>
+        </div>
+        );
+    };
+
+    const OccupationalHealthCareEntry = ({ entry }: { entry: OccupationalHealthCareEntry }) => {
+        return (
+            <div>
+                <Table>
+                    <Table.Body>
+                      <ul style={{marginTop:15}} key={entry.id}>
+                    <Table.Row>
+                        <li>Type: {entry.type} <Icon style={{marginLeft:10}} name='factory' size="large"/></li>
+                    </Table.Row>
+                    <Table.Row>
+                        <li>Date: {entry.date}</li>
+                    </Table.Row>
+                    <Table.Row>
+                        <li>Description: {entry.description}</li>
+                        {entry.diagnosisCodes && <li>Diagnosis codes: {diagnosisCodes (entry.diagnosisCodes)}</li>}
+                    </Table.Row>
+                 </ul>
+                </Table.Body>
+            </Table>
+        </div>
+        );
+    };
+
+    const HealthCheckEntry = ({ entry }: { entry: HealthCheckEntry }) => {
+
+
+        const color = (input:number): "orange"|"green" =>  {
+            if (input==0) {
+                return "orange";
+            } else {
+                return "green";
+            }
+        };
+
+        return (
+            <div>
+                <Table>
+                    <Table.Body>
+                    <ul style={{marginTop:15}} key={entry.id}>
+                        <Table.Row>
+                            <li>Type: {entry.type} <Icon style={{marginLeft:10}} name='clock outline' size="large"/></li>
+                        </Table.Row>
+                        <Table.Row>
+                            <li>Date: {entry.date}</li>
+                        </Table.Row>
+                        <Table.Row>
+                            <li>Description: {entry.description}</li>
+                        </Table.Row>
+                        <Table.Row>
+                            <li>Health check rating: {entry.healthCheckRating} <Icon name="heart" color={color(entry.healthCheckRating)}/></li>
+                        </Table.Row>
+                    </ul>
+                    </Table.Body>
+                </Table>
+            </div>
+        );
+    };
+
+    const EntryDetails = ({ entry }: { entry: Entry })  => {
+        switch (entry.type) {
+            case "HealthCheck":
+                return (
+                    <div>
+                        <HealthCheckEntry entry={entry} />
+                    </div>
+                );            
+            case "OccupationalHealthcare":
+                return (
+                    <div>
+                        <OccupationalHealthCareEntry entry={entry} />
+                    </div>
+                );               
+            case "Hospital":
+                    return (
+                        <div>
+                            <HospitalEntry entry={entry} />
+                        </div>
+                    );                  
+            default:
+            return assertNever(entry);
+        }
+    };
+
+    const assertNever = (value: never): never => {
+        throw new Error(
+          `Unhandled discriminated union member: ${JSON.stringify(value)}`
+        );
+      };
+
     return (
         <div>
+            <Container>
             <h1>{patient.name} {patient.gender=='male' && <Icon name='mars'/>} {patient.gender=='female' && <Icon name='venus'/>}</h1>
             ssn: {patient.ssn}<br></br>
             occupation: {patient.occupation}
             <br></br><br></br>
-            {patient.entries.length>0 && <div style={{marginBottom:-10}}><b>Entries:</b></div>}
+            {patient.entries.length>0 && <div style={{marginBottom:10}}><h4><b>Entries</b></h4></div>}
             {patient.entries.map(entry => 
-                <ul style={{marginTop:15}} key={entry.id}>
-                 <li>Date: {entry.date}</li>
-                 <li>Type: {entry.type}</li>
-                 <li>Description: {entry.description}</li>
-                 {/* {entry.diagnosisCodes && <li>Diagnosis codes: {entry.diagnosisCodes}</li>} */}
-                 {entry.diagnosisCodes && <li>Diagnosis codes: {diagnosisCodes (entry.diagnosisCodes)}</li>}
-            </ul>
+                <p key={entry.id}>
+                    <EntryDetails entry={entry} />
+                </p>
             )}
+            </Container>
         </div>
     );
 };
